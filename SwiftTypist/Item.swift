aa866e19761a0877e93c4b9d0f84eaf3015c12b8
Jordan Christensen <jordan.c4922@gmail.com>
2025-08-16 13:49:20 -0600

    Initial Commit

---
+++ SwiftTypist/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftTypist
+//
+//  Created by Jordan Christensen on 8/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwiftTypistTests/SwiftTypistTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTypistTests.swift
+//  SwiftTypistTests
+//
+//  Created by Jordan Christensen on 8/16/25.
+//
+
+import Testing
+@testable import SwiftTypist
+
+struct SwiftTypistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
