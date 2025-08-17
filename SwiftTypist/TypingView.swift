72029ed4335d3f6474e37f8056b560881713dda5
Jordan Christensen <jordan.c4922@gmail.com>
2025-08-17 16:54:01 -0600

    Add README.md and LICENSE to the project

--- SwiftTypist/TypingView.swift
+++ SwiftTypist/TypingView.swift
@@ -41,7 +41,7 @@ struct CircularString: ExpressibleByStringLiteral {
 }
 
 struct TypingView: View {
-    private let rollingText: CircularString = "As a child, I watched superhero movies and wanted to be one myself. After coming across a vile of gold flakes in my dad's office and knowing that powers came from interesting circumstances, I popped open the vile and dumped its contents down my throat. "
+    private let rollingText: CircularString = "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: "
     @State private var workingText = ""
     @State private var currentIndex = 0
     
@@ -60,7 +60,6 @@ struct TypingView: View {
                 
                 Text(beforeCurrentLetter)
                     .foregroundStyle(Color.writtenText)
-                
                 
                 if currentCharacter == " " {
                     Text("_")
